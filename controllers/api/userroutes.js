8652a4392ca1f46162b87e88f764307c9d5124fc
Maung Htike <maunghtike1999@gmail.com>
2022-05-10 00:00:58 -0700

    add comments

--- controllers/api/userroutes.js
+++ controllers/api/userroutes.js
@@ -23,24 +23,27 @@ router.post('/', async (req, res) => {
 // login
 router.post('/login', async (req, res) => {
     try {
+        // searches db for the username
         const dbUserData = await User.findOne({
             where: {
                 username: req.body.username,
             },
         });
 
+        // if username is not found, return incorrect input
         if (!dbUserData) {
             res.status(400).json({ message: 'Incorrect username or password. Please try again!' });
             return;
         }
 
+        // checks if password is equal to hashed password in db
         const passwordCheck = await dbUserData.checkPassword(req.body.password);
-
         if (!passwordCheck) {
             res.status(400).json({ message: 'Incorrect email or password. Please try again!' });
             return;
         }
 
+        // sets session as user logged in
         req.session.save(() => {
             req.session.loggedIn = true;
 

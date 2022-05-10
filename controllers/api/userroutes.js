8987cf584c553644b24b32417be3f619fc5a7b77
Maung Htike <maunghtike1999@gmail.com>
2022-05-09 23:54:38 -0700

    add create new user function to model

---
+++ controllers/api/userroutes.js
@@ -0,0 +1,23 @@
+// dependencies
+const router = require('express').Router();
+const { User } = require('../../models');
+
+// create new user
+router.post('/', async (req, res) => {
+    try {
+        const dbUserData = await User.create({
+            username: req.body.username,
+            password: req.body.password,
+        });
+
+        req.session.save(() => {
+            req.session.loggedIn = true;
+
+            res.status(200).json(dbUserData);
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+

30d5cd6ce02a158b68b638f9a75543b898c794ba
Maung Htike <maunghtike1999@gmail.com>
2022-05-09 20:24:27 -0700

    add login authenticator

---
+++ utils/auth.js
@@ -0,0 +1,15 @@
+// authentication middleware to check if user is logged in
+const withAuth = (req, res, next) => {
+
+    // If the user is not logged in, redirect the user to the login page
+    if (!req.session.loggedIn) {
+        res.redirect('/login');
+    } else {
+
+        // If the user is logged in, continue on to the rest of the route
+        // We call next() if the user is authenticated
+        next();
+    }
+};
+
+module.exports = withAuth;

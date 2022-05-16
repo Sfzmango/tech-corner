554cf0870bca78efe32aeed778e01035a3b5a118
Maung Htike <maunghtike1999@gmail.com>
2022-05-15 19:13:34 -0700

    fix typo on server.js

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const path = require('path');
 
 const routes = require('./controllers');
-const sequelize = require('.config/');
+const sequelize = require('./config/connections');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({});
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
